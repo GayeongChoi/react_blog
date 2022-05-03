6c976e18ff6e536b38476992578be5331bfc6f88
GayeongChoi <wwuu12366@gmail.com>
2022-05-03 19:39:03 +0900

    22.05.03

--- .Prettierrc.js
+++ .Prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     endOfLine: "lf",
     tabWidth: 4,
     semi: true,
-    singleQuote: false,
+    singleQuote: true,
     trailingComma: "all", //끝날 때 다 컴마를 줌
     printWidth: 120, //줄이 120자 되면 자동 줄 바꿈
 };
